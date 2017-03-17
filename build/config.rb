77dcc6f97e1d21603a684eb483e0914dfe59aaa7
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2017-03-17 23:53:06 +0000

    Adding stylesheets

---
+++ build/config.rb
@@ -0,0 +1,19 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "build/css"
+sass_dir = "source/sass"
+images_dir = "source/img"
+javascripts_dir = "source/js"
+fonts_dir = "source/css/fonts"
+
+output_style = :nested
+
+relative_assets = false
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :sass
